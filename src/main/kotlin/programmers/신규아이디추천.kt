ac48e8f9026d29fd9938541834693e89b674395b
dddooo9 <dlaehdus99@naver.com>
2021-12-16 21:36:18 +0900

    [feat] 프로그래머스 : 신규 아이디 추천

---
+++ src/main/kotlin/programmers/신규아이디추천.kt
@@ -0,0 +1,38 @@
+package programmers
+
+class Solution {
+    fun solution(new_id: String): String {
+        val special = "-_.~!@#\\$%^&*()=+[{]}:?,<>/"
+
+        var recommendId = new_id.toLowerCase()
+            .filter { it.isLowerCase() || it.isDigit() || it == '-' || it == '_' || it == '.' }
+            .let {
+                var tempId = it
+                while (tempId.contains("..")) {
+                    tempId = tempId.replace("..", ".")
+                }
+
+                tempId
+            }
+            .removePrefix(".")
+            .removeSuffix(".")
+            .let { it.ifEmpty { "a" } }
+            .let {
+                if (it.length >= 16) {
+                    it.substring(0..14)
+                } else it
+            }
+            .removeSuffix(".")
+            .let {
+                var tempId = it
+                if (tempId.length <= 2) {
+                    while (tempId.length != 3) {
+                        tempId += tempId.last()
+                    }
+                }
+                tempId
+            }
+
+        return recommendId
+    }
+}
